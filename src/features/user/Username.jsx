c4b2687535ffd2caf5295c7e27a75d67d7a24d23
eliaquim.daluz@yahoo.com <eliaquim.daluz@yahoo.com>
2024-06-14 17:54:48 +0100

    P13

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Eliaquim</div>;
+  return <div className="hidden text-sm font-semibold md:block">Eliaquim</div>;
 }
 
 export default Username;
