17c2bdcc0607e8b04282013aac35a2fcc44d0b40
Eliaquim da Luz <eliaquim.daluz@gmail.com>
2024-06-16 17:37:09 +0100

    P14

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,18 +1,20 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utilities/helpers";
 
-function CartItem() {
-    //const { pizzaId, name, quantity, totalPrice } = item;
+function CartItem({ item }) {
+  const { name, quantity, totalPrice } = item;
 
-    return (
-        <li>
-            <p>
-                {}&times; {name}
-            </p>
-            <div>
-                <p>{formatCurrency(1589)}</p>
-            </div>
-        </li>
-    );
+  return (
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
+        {quantity} &times; {name}
+      </p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
+      </div>
+    </li>
+  );
 }
 
 export default CartItem;
---
+++ src/ui/Button.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-yellow-300 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "px-4 py-2.5 md:px-6 md:py-3.5 inline-block text-sm rounded-full bg-transparent border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 hover:text-stone-800 transition-colors duration-300 hover:bg-stone-300 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-stone-200 disabled:cursor-not-allowed focus:text-stone-800",
+  };
+  if (to)
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
